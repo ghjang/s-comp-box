8cfee940ecf664088d01a72201ca71563370066b
ghjang <jmxexpert@hotmail.com>
2024-04-16 17:44:53 +0900

    롤업 번들링시에 발생하는 불필요 경고가 나타나지 않게 함. 롤업 설정 파일내 주석 참고할 것.

--- rollup.config.js
+++ rollup.config.js
@@ -38,7 +38,20 @@ function createConfig(name, outputFilename, customElement = false) {
                 dedupe: ['svelte']
             }),
             production && terser()
-        ]
+        ],
+        onwarn: function (warning, warn) {
+            // FIXME: 1개의 '.svelte' 파일로 '보통의 스벨트 컴포넌트 번들링'과 '표준 웹 커스텀 컴포넌트 번들링'을 모두 하는 경우에 발생하는 경고를 무시한다.
+            //        이 경고는 '.svelte' 파일에 '<svelte:options customElement="s-marquee" />' 옵션이 설정되어 있는 상태에서
+            //        'customElement: false'로 설정하여 번들링을 할 때 발생한다. 현재 시점에서 이 경고는 무시해도 될 것으로 보여 번들링 경고에서 제외한다.
+            if (!customElement
+                && warning.code === 'PLUGIN_WARNING'
+                && warning.plugin === 'svelte'
+                && warning.message.includes('The \'customElement\' option is used when generating a custom element.')) {
+                return;
+            }
+
+            warn(warning);
+        }
     };
 }
 
