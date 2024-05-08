06ab22d0f32214b36518928e248a565da048cf1e
ghjang <jmxexpert@hotmail.com>
2024-05-08 22:50:46 +0900

    - BubbleBox 컴포넌트 초기 버전 추가
    - NOTE: '웹 컴포넌트' 방식으로 사용할 경우에 컴포넌트의 CSS 설정은 'Shadow DOM'내에 삽입된다고 함.
            결과적으로 출력되는 '.custom.css' 파일에 아무것도 출력되지 않음. 웹 컴포넌트 방식으로
            테스트하는 코드에서 '.custom.css' 파일을 포함시키는 코드 제거함.

---
+++ src/BubbleBox/Bubble.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let message = "";
+  export let sender = "user";
+</script>
+
+<div class="bubble {sender}">
+  {#if message}
+    {message}
+  {:else}
+    <slot />
+  {/if}
+</div>
+
+<style>
+  .bubble {
+    width: fit-content;
+    max-width: 80%;
+    padding: 10px;
+    margin: 5px;
+    border-radius: 10px;
+    color: white;
+  }
+
+  .user {
+    align-self: flex-end;
+    background-color: #0b93f6;
+  }
+
+  .bot {
+    align-self: flex-start;
+    background-color: #e5e5ea;
+    color: black;
+  }
+</style>
---
+++ src/BubbleBox/BubbleBox.svelte
@@ -0,0 +1,20 @@
+<svelte:options customElement="s-bubble-box" />
+
+<script>
+  import Bubble from "./Bubble.svelte";
+
+  export let bubbles = [];
+</script>
+
+<div class="bubble-box">
+  {#each bubbles as bubble}
+    <Bubble {...bubble} />
+  {/each}
+</div>
+
+<style>
+  .bubble-box {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
