8238dfdd53ee8d9e22ee98ba525806031bb4234e
ghjang <jmxexpert@hotmail.com>
2024-05-10 12:02:15 +0900

    BubbleBox ==> FlexBox

---
+++ src/FlexBox/FlexBox.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Bubble from "./Bubble.svelte";
+
+  export let direction = "column";
+  export let items = [];
+
+  let flexBox;
+</script>
+
+<div class="flex-box" bind:this={flexBox} style:flex-direction={direction}>
+  {#each items as item}
+    {@const type = item.type}
+    {@const deleted = delete item.type}
+    {#if type === "bubble"}
+      <Bubble {...item} />
+    {:else if deleted}
+      {@html JSON.stringify({ type: type, ...item })}
+    {/if}
+  {/each}
+</div>
+
+<style>
+  .flex-box {
+    display: flex;
+  }
+</style>
