75dfcff1b11d869065586a9d4193ff7d134df420
ghjang <jmxexpert@hotmail.com>
2024-05-28 14:02:46 +0900

    WebView 컴포넌트에 복잡한 내용의 페이지 로딩시 Splitter 그립을 제대로 드래깅할 수 없는 문제 workaround 처리함.

--- src/Splitter/dragGrip.js
+++ src/Splitter/dragGrip.js
@@ -1,6 +1,6 @@
 export function dragGrip(node, initialParams) {
     let params = initialParams;
-    
+
     let isDragging = false;
     let startX, startY, startWidth, startHeight;
 
@@ -13,15 +13,6 @@ export function dragGrip(node, initialParams) {
 
         isDragging = true;
 
-        if (params.panel_0.firstElementChild) {
-            panel_0_display = params.panel_0.firstElementChild.style.display;
-            params.panel_0.firstElementChild.style.display = "none";
-        }
-        if (params.panel_1.firstElementChild) {
-            panel_1_display = params.panel_1.firstElementChild.style.display;
-            params.panel_1.firstElementChild.style.display = "none";
-        }
-
         if (params.direction === "horizontal") {
             startX = event.clientX;
             startWidth = params.panel_0.offsetWidth;
@@ -29,11 +20,50 @@ export function dragGrip(node, initialParams) {
             startY = event.clientY;
             startHeight = params.panel_0.offsetHeight;
         }
+
+        // '드래깅' 중에 패널 컨텐트 숨김
+        if (params.hidePanel) {
+            if (params.panel_0.firstElementChild) {
+                panel_0_display = params.panel_0.firstElementChild.style.display;
+                params.panel_0.firstElementChild.style.display = "none";
+            }
+            if (params.panel_1.firstElementChild) {
+                panel_1_display = params.panel_1.firstElementChild.style.display;
+                params.panel_1.firstElementChild.style.display = "none";
+            }
+        }
+
+        // NOTE: 'WebView'등의 컴포넌트에 '복잡한 내용', 특히 '마우스 이동' 이벤트를 처리하는 로직이
+        //       포함되어 있는 경우 '스플리터 그립'쪽에 이벤트가 전달되지 않아 '그립 드래깅'이 제대로
+        //       동작하지 않은 문제 workaround함.
+        //
+        // 명시적으로 'false'로 설정한 경우가 아닌 경우에 '탑 레벨 더미 레이어'를 사용하여
+        // '마우스 이동' 이벤트를 캡처하도록 함.
+        if (params.useTopLevelLayerMouseMoveEventCapture !== false) {
+            const layerDiv = document.createElement("div");
+            layerDiv.style.cssText = `
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100vw;
+                height: 100vh;
+                margin: 0;
+                padding: 0;
+                z-index: 9999;
+            `;
+            layerDiv.id = "top-level-layer-for-mousemove-event-capture";
+            layerDiv.addEventListener("mousemove", handleMousemove);
+            node.appendChild(layerDiv);
+        }
     }
 
     function handleMousemove(event) {
         if (!isDragging) {
             return;
+        }
+
+        if (params.useTopLevelLayerMouseMoveEventCapture !== false) {
+            event.stopPropagation();
         }
 
         if (params.direction === "horizontal") {
@@ -48,13 +78,24 @@ export function dragGrip(node, initialParams) {
     function handleMouseup() {
         isDragging = false;
 
-        if (params.panel_0.firstElementChild) {
-            params.panel_0.firstElementChild.style.display = panel_0_display;
-            panel_0_display = null;
+        // 숨겼던 패널 컨텐트 복원
+        if (params.hidePanel) {
+            if (params.panel_0.firstElementChild) {
+                params.panel_0.firstElementChild.style.display = panel_0_display;
+                panel_0_display = null;
+            }
+            if (params.panel_1.firstElementChild) {
+                params.panel_1.firstElementChild.style.display = panel_1_display;
+                panel_1_display = null;
+            }
         }
-        if (params.panel_1.firstElementChild) {
-            params.panel_1.firstElementChild.style.display = panel_1_display;
-            panel_1_display = null;
+
+        if (params.useTopLevelLayerMouseMoveEventCapture !== false) {
+            const layerDiv = document.getElementById("top-level-layer-for-mousemove-event-capture");
+            if (layerDiv) {
+                layerDiv.removeEventListener("mousemove", handleMousemove);
+                layerDiv.remove();
+            }
         }
     }
 
