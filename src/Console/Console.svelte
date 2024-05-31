d573fe01d22a0f9639b2f3ea2dd3b6b5e94d9739
ghjang <jmxexpert@hotmail.com>
2024-05-31 21:44:39 +0900

    콘솔 'clear' 메쏘드 추가, 'initialOutput' 속성 추가

--- src/Console/Console.svelte
+++ src/Console/Console.svelte
@@ -1,13 +1,34 @@
 <script>
+  import { onMount } from "svelte";
+
   export let height = "100%";
 
+  // TODO: 여러 유형의 '콘솔 출력' 정의
+  //
+  // 현재 '문자열'을 입력하는 것으로만 처리하고 있음. 다음과 같은 유형을 지원할 수 있을 것 같음:
+  // - 문자열
+  // - 객체
+  // - 배열
+  // - 유용한 특수 데이터에 대한 출력 포맷 (예: 표, 그래프, 이미지)
+  
+  export let initialOutput = null;
+
   let consoleDiv;
+
+  export function clear() {
+    if (!consoleDiv) {
+      return;
+    }
+
+    consoleDiv.innerHTML = "";
+  }
 
   export function log(output, autoScrollDown = true) {
     if (!consoleDiv) {
       return;
     }
 
+    output = processNewLine(output);
     consoleDiv.innerHTML += `<div>${output}</div>`;
     if (autoScrollDown) {
       consoleDiv.scrollTop = consoleDiv.scrollHeight;
@@ -19,11 +40,22 @@
       return;
     }
 
+    output = processNewLine(output);
     consoleDiv.innerHTML += `<div style="color: red;">${output}</div>`;
     if (autoScrollDown) {
       consoleDiv.scrollTop = consoleDiv.scrollHeight;
     }
   }
+
+  function processNewLine(output) {
+    return output.replace(/\n/g, "<br>");
+  }
+
+  onMount(() => {
+    if (initialOutput) {
+      log(initialOutput);
+    }
+  });
 </script>
 
 <div class="console-output" bind:this={consoleDiv} style:height></div>
--- test/Console/Console.js
+++ test/Console/Console.js
@@ -4,6 +4,7 @@ import Console from '/build/dev/default/Console.js';
 const customConsole = new Console({
     target: document.getElementById('container'),
     props: {
+        initialOutput: 'Hello~\nThis is a custom console.\n\n'
     }
 });
 
@@ -14,5 +15,8 @@ for (let i = 0; i < 3; ++i) {
 for (let i = 0; i < 5; ++i) {
     customConsole.error("This is an error message.");
 }
+
+const clearBtn = document.getElementById('clearConsoleBtn');
+clearBtn.addEventListener('click', () => customConsole.clear());
 
 window.addEventListener("beforeunload", () => customConsole?.$destroy());
