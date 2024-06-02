bc0ffee02e375c107af0a7715bc664714091221a
ghjang <jmxexpert@hotmail.com>
2024-06-02 14:40:23 +0900

    트랙패드 좌/우 이동시에도 오버스크롤 방지하도록 수정

--- src/common/action/preventOverscroll.js
+++ src/common/action/preventOverscroll.js
@@ -3,12 +3,25 @@ export function preventOverscroll(node) {
         // 스크롤 방향이 아래이고 스크롤이 맨 아래에 도달한 경우
         const isScrollingDownAtBottom =
             event.deltaY > 0 &&
-            node.scrollHeight - node.scrollTop === node.clientHeight;
+            Math.ceil(node.scrollTop) + node.clientHeight >= node.scrollHeight;
 
         // 스크롤 방향이 위이고 스크롤이 맨 위에 도달한 경우
         const isScrollingUpAtTop = event.deltaY < 0 && node.scrollTop === 0;
 
-        if (isScrollingDownAtBottom || isScrollingUpAtTop) {
+        // 스크롤 방향이 오른쪽이고 스크롤이 맨 오른쪽에 도달한 경우
+        const isScrollingRightAtEnd =
+            event.deltaX > 0 &&
+            Math.ceil(node.scrollLeft) + node.clientWidth >= node.scrollWidth;
+
+        // 스크롤 방향이 왼쪽이고 스크롤이 맨 왼쪽에 도달한 경우
+        const isScrollingLeftAtStart = event.deltaX < 0 && node.scrollLeft === 0;
+
+        if (
+            isScrollingDownAtBottom ||
+            isScrollingUpAtTop ||
+            isScrollingRightAtEnd ||
+            isScrollingLeftAtStart
+        ) {
             event.preventDefault();
         }
     }
