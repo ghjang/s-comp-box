6a437c0c79b9962c7146d5ca9e1b5be32c70b3d2
ghjang <jmxexpert@hotmail.com>
2024-06-05 22:28:52 +0900

    '크롬 확장'에서 실행될 경우 고려 ; 크롬 확장에서 '웹 어셈블리' 모듈을 사용할 수 없다. 외부에 퍼블리슁된 테스트 페이지를 로딩하도록 한다.

--- test/PyRun/PyRun.webview.js
+++ test/PyRun/PyRun.webview.js
@@ -1,10 +1,13 @@
 
 import WebView from '/build/dev/default/WebView.js';
 
+const urlPrefix = window.location.protocol === 'chrome-extension:' ? 'https://ghjang.github.io/s-comp' : '';
+const url = `${urlPrefix}/test/PyRun/PyRun.custom.html`;
+
 const webView = new WebView({
     target: document.getElementById('container'),
     props: {
-        url: "https://ghjang.github.io/s-comp/test/PyRun/PyRun.custom.html"
+        url
     }
 });
 
