d4a60e133a22ea0ac01c949bd8ad890369c12b99
ghjang <jmxexpert@hotmail.com>
2024-06-07 22:25:19 +0900

    주석 추가

--- test/s-comp.mapping.js
+++ test/s-comp.mapping.js
@@ -33,6 +33,12 @@ async function loadAndRegisterComponents() {
             //       'module.default', 여기서 '클래스 명'이 'production' 빌드환경에서
             //       원래의 클래스 이름이 아닌 '단축된 이름'으로 변경되기 때문에 클래스 이름 문자열을
             //       첫번째 인자로 명시적으로 전달한다.
+            //
+            //       참고로 'import XXX from "YYY";'와 같은 형태로 사용할 경우에
+            //       'XXX' 부분은 'default export' 부분을 '이름을 XXX 바꾸어 참조'한다는 의미이다.
+            //       즉 'module.default'에 대한 'alias' 지정 문법이라는 것이다.
+            //       단순히 alias를 지정하는 것이기 때문에 엉뚱한 이름으로 지정할 수 있는 경우가 있어
+            //       주의할 필요가 있다.
             gSCompMapper.registerComponent(componentName, module.default);
         } catch (error) {
             console.error(`Failed to load component ${componentName}:`, error);
