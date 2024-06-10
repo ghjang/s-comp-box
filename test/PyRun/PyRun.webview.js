efdfb69a14e23b09fc4a9b01c59b208450cc164d
ghjang <jmxexpert@hotmail.com>
2024-06-10 21:07:14 +0900

    fixed broken link

--- test/PyRun/PyRun.webview.js
+++ test/PyRun/PyRun.webview.js
@@ -1,7 +1,7 @@
 
 import WebView from '/build/dev/default/WebView.js';
 
-const urlPrefix = window.location.protocol === 'chrome-extension:' ? 'https://ghjang.github.io/s-comp' : 's-comp';
+const urlPrefix = window.location.protocol === 'chrome-extension:' ? 'https://ghjang.github.io/s-comp' : '';
 const url = `${urlPrefix}/test/PyRun/PyRun.custom.html`;
 
 const webView = new WebView({
