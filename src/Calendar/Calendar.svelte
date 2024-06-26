79c05fd4c5d0209b1439e67c6e8d45cd859fc875
ghjang <jmxexpert@hotmail.com>
2024-06-26 19:18:42 +0900

    현재의 구현에서 'in:fly' 애니메이션이 종료되는 시점에 간혹 새로 생성되는 '일자 div' 부분이 잠깐 100% Y축 translate된 상태로 있는 문제 보완

--- src/Calendar/Calendar.svelte
+++ src/Calendar/Calendar.svelte
@@ -150,142 +150,175 @@
   // - '오늘' 날짜에 '원' 보더 표시
 </script>
 
-<div
-  class="calendar"
-  style:max-height={calendarSize ? `${calendarSize.height}px` : "auto"}
-  bind:this={calendar}
->
-  <div class="header">
-    <div class="monthYear">{monthNames[selectedMonth - 1]} {selectedYear}</div>
-    <button
-      class="prevMonth"
-      disabled={xDirection !== 0}
-      on:click={handlePrevMonthClick}>&lt;</button
-    >
-    <button
-      class="nextMonth"
-      disabled={xDirection !== 0}
-      on:click={handleNextMonthClick}>&gt;</button
-    >
-    {#each dayNamesOfWeek as dayName}
-      <div class="dayName">{dayName}</div>
-    {/each}
-  </div>
-  {#key `${selectedYear}-${selectedMonth}`}
-    <div
-      class="dayNumbers"
-      class:isFlying={xDirection !== 0}
-      in:fly={{
-        x: xDirection * calendarSize.width,
-        duration: animationDuration,
-      }}
-      out:fly={flyOutProp}
-      on:introend={() => (xDirection = 0)}
-      on:outroend={() => (xDirection = 0)}
-    >
-      {#each dayNumbers as { day, key } (key)}
+<div class="calendar-container">
+  <div
+    class="calendar"
+    style:max-height={calendarSize ? `${calendarSize.height}px` : "auto"}
+    bind:this={calendar}
+  >
+    <div class="top">
+      <div class="header">
+        <div class="monthYear">
+          {monthNames[selectedMonth - 1]}
+          {selectedYear}
+        </div>
         <button
-          tabindex="-1"
-          class="dayNumber"
-          class:hoverable={day}
-          class:selected={selectedDayNumber === day}
-          on:click={handleDayNumberClick}
+          class="prevMonth"
+          disabled={xDirection !== 0}
+          on:click={handlePrevMonthClick}>&lt;</button
         >
-          {day || ""}
-        </button>
-      {/each}
+        <button
+          class="nextMonth"
+          disabled={xDirection !== 0}
+          on:click={handleNextMonthClick}>&gt;</button
+        >
+        {#each dayNamesOfWeek as dayName}
+          <div class="dayName">{dayName}</div>
+        {/each}
+      </div>
     </div>
-  {/key}
+    <div class="bottom">
+      {#key `${selectedYear}-${selectedMonth}`}
+        <div
+          class="dayNumbers"
+          class:isFlying={xDirection !== 0}
+          in:fly={{
+            x: xDirection * calendarSize.width,
+            duration: animationDuration
+          }}
+          out:fly={flyOutProp}
+          on:introend={() => (xDirection = 0)}
+        >
+          {#each dayNumbers as { day, key } (key)}
+            <button
+              tabindex="-1"
+              class="dayNumber"
+              class:hoverable={day}
+              class:selected={selectedDayNumber === day}
+              on:click={handleDayNumberClick}
+            >
+              {day || ""}
+            </button>
+          {/each}
+        </div>
+      {/key}
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
   $grid-item-padding: 10px;
+  $bg-color: white;
 
-  .calendar {
-    overflow: hidden;
-    user-select: none;
+  .calendar-container {
+    border: 1px solid darkgray;
+    border-radius: 5px;
+    padding: 5px;
+    background-color: $bg-color;
 
-    .header {
-      display: grid;
-      grid-template-rows: repeat(2, 1fr);
-      grid-template-columns: repeat(7, 1fr);
-      text-align: center;
-      align-items: center;
+    .calendar {
+      position: relative;
+      z-index: 0;
+      background-color: $bg-color;
+      overflow: hidden;
+      user-select: none;
 
-      .monthYear {
-        grid-column: 1 / -3;
-        padding: $grid-item-padding;
-        font-size: 1.2em;
-        font-weight: bold;
-      }
+      .top {
+        position: relative;
+        z-index: 2;
+        background-color: $bg-color;
 
-      .prevMonth,
-      .nextMonth {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: $grid-item-padding;
-        width: 1.7em;
-        height: 1.7em;
-        font-size: 1.2em;
-        font-weight: bold;
-        background: none;
-        border: none;
-        border-radius: 50%;
-        outline: none;
-        cursor: pointer;
+        .header {
+          display: grid;
+          z-index: 1;
+          grid-template-rows: repeat(2, 1fr);
+          grid-template-columns: repeat(7, 1fr);
+          text-align: center;
+          align-items: center;
 
-        &:hover {
-          background-color: lighten(darkgray, 20%);
-        }
-
-        &:focus {
-          background-color: darkgray;
-
-          &:hover {
-            background-color: darken(darkgray, 10%);
-          }
-        }
-      }
-
-      .dayName {
-        padding: $grid-item-padding;
-      }
-    }
-
-    .dayNumbers {
-      display: grid;
-      grid-template-rows: repeat(6, 1fr);
-      grid-template-columns: repeat(7, 1fr);
-      text-align: center;
-      align-items: center;
-
-      .dayNumber {
-        background: none;
-        border: none;
-        padding: $grid-item-padding;
-        outline: none;
-
-        &.hoverable {
-          border-radius: 50%;
-          cursor: pointer;
-
-          &:hover {
-            background-color: lighten(lightcoral, 20%);
+          .monthYear {
+            grid-column: 1 / -3;
+            padding: $grid-item-padding;
+            font-size: 1.2em;
+            font-weight: bold;
           }
 
-          &.selected {
-            background-color: lightcoral;
+          .prevMonth,
+          .nextMonth {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: $grid-item-padding;
+            width: 1.7em;
+            height: 1.7em;
+            font-size: 1.2em;
+            font-weight: bold;
+            background: none;
+            border: none;
+            border-radius: 50%;
+            outline: none;
+            cursor: pointer;
 
             &:hover {
-              background-color: darken(lightcoral, 10%);
+              background-color: lighten(darkgray, 20%);
             }
+
+            &:focus {
+              background-color: darkgray;
+
+              &:hover {
+                background-color: darken(darkgray, 10%);
+              }
+            }
+          }
+
+          .dayName {
+            padding: $grid-item-padding;
           }
         }
       }
 
-      &.isFlying:last-child {
-        transform: translateY(-100%);
+      .bottom {
+        position: relative;
+        z-index: 1;
+        background-color: $bg-color;
+
+        .dayNumbers {
+          display: grid;
+          z-index: 0;
+          grid-template-rows: repeat(6, 1fr);
+          grid-template-columns: repeat(7, 1fr);
+          text-align: center;
+          align-items: center;
+
+          .dayNumber {
+            background: none;
+            border: none;
+            padding: $grid-item-padding;
+            outline: none;
+
+            &.hoverable {
+              border-radius: 50%;
+              cursor: pointer;
+
+              &:hover {
+                background-color: lighten(lightcoral, 20%);
+              }
+
+              &.selected {
+                background-color: lightcoral;
+
+                &:hover {
+                  background-color: darken(lightcoral, 10%);
+                }
+              }
+            }
+          }
+
+          &.isFlying:not(:first-child):last-child {
+            transform: translateY(-100%);
+          }
+        }
       }
     }
   }
