5a525c376589c9b5ec615f1ffe73de8cc70320b0
ghjang <jmxexpert@hotmail.com>
2024-07-09 13:10:37 +0900

    컴포넌트 로딩 코드 수정

--- src/SCompBox/SCompBox.svelte
+++ src/SCompBox/SCompBox.svelte
@@ -1,7 +1,7 @@
 <svelte:options customElement="s-comp-box" />
 
 <script>
-  import { loadScript } from "../common/util.js";
+  import { loadScript, loadClassFromModule } from "../common/util.js";
   import SCompInfo from "./SCompInfo.svelte";
   import Floor from "../Floor/Floor.svelte";
 
@@ -18,19 +18,28 @@
   $: if (sCompInfo) {
     const customContainers = sCompInfo.getAvailableCustomContainers(menuItems);
 
-    sCompInfo.getAvailableCustomElements().then((customElements) => {
-      loadCustomElementsInfo(customContainers, customElements);
-    });
+    sCompInfo
+      .getAvailableCustomElements()
+      .then((customElements) => {
+        return loadCustomElementsInfo(customContainers, customElements);
+      })
+      .then((_menuItems) => {
+        menuItems = _menuItems;
+      })
+      .catch((error) => {
+        console.error("Failed to load custom elements info: ", error);
+      });
   }
 
   // FIXME: 현재 '탑 레벨' 메뉴에서만 '컴포넌트 설정'이 있는 것을 가정하고 있다.
-  function loadCustomElementsInfo(customContainers, customElements) {
+  async function loadCustomElementsInfo(customContainers, customElements) {
     const divider = { divider: { style: "" } };
     const items = [...customContainers, divider, ...customElements];
+    const _menuItems = [];
 
-    items.forEach((item) => {
+    for (const item of items) {
       if (item.divider || item.link || item.popup || item.subMenu) {
-        menuItems.push(item);
+        _menuItems.push(item);
       } else if (item.component) {
         const comp = item.component;
         let props = comp.props || {};
@@ -39,39 +48,75 @@
           ? comp.componentClass.name
           : null;
         if (constructorName && compProps[constructorName]) {
+          // 설정에서 직접 '컴포넌트 클래스'를 지정한 경우
+
+          // 사용자 설정이 존재할 경우에 사용자 설정으로 오버라이드
           props = { ...props, ...compProps[constructorName] };
 
           // 'normal'로 번들링된 컴포넌트
           if (compJsBundleBasePath) {
             const scriptPath = `${compJsBundleBasePath}/${constructorName}.js`;
-            loadScript(scriptPath);
+            await loadScript(scriptPath);
           }
         } else if (comp.name && compProps[comp.name]) {
+          // '컴포넌트 문자열 이름'으로 설정한 경우
+
+          // 사용자 설정이 존재할 경우에 사용자 설정으로 오버라이드
           props = { ...props, ...compProps[comp.name] };
 
           // 'custom'으로 번들링된 컴포넌트
           if (customCompJsBundleBasePath) {
-            const scriptPath = `${customCompJsBundleBasePath}/${comp.name}.custom.js`;
-            loadScript(scriptPath);
+            let scriptPath = `${customCompJsBundleBasePath}/${comp.name}.custom.js`;
+            try {
+              await loadScript(scriptPath);
+            } catch (error) {
+              console.warn(
+                `failed to load the custom component script: ${scriptPath}, error: `,
+                error
+              );
+
+              // 'custom'으로 번들링된 컴포넌트가 없을 경우 'normal'로 번들링된 컴포넌트 시도
+              if (compJsBundleBasePath) {
+                scriptPath = `${compJsBundleBasePath}/${comp.name}.js`;
+                await loadScript(scriptPath);
+                comp.componentClass = await loadClassFromModule(
+                  scriptPath,
+                  comp.name
+                );
+              }
+            }
           }
+        } else {
+          // Do nothing
         }
 
-        menuItems.push({
+        if (!comp.componentClass && !comp.customElementName) {
+          console.error(
+            "No proper component class or custom element name: ",
+            comp
+          );
+          throw new Error(
+            "No proper component class or custom element name: ",
+            comp
+          );
+        }
+
+        _menuItems.push({
           action: {
             text: comp.description,
             handler: () => {
               return {
-                customElementName: comp.customElementName,
                 componentClass: comp.componentClass,
+                customElementName: comp.customElementName,
                 props,
               };
             },
           },
         });
-      } else {
-        // Do nothing
       }
-    });
+    }
+
+    return _menuItems;
   }
 </script>
 
