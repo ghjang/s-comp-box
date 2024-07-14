29f6b04df6b82cd85f7f3a65569342b1ee88efdc
ghjang <jmxexpert@hotmail.com>
2024-07-14 17:08:59 +0900

    AbcRun 컴포넌트 초기 코드 추가

---
+++ src/AbcRun/AbcRun.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Splitter from "../Splitter/Splitter.svelte";
+  import MonacoEditor from "../MonacoEditor/MonacoEditor.svelte";
+</script>
+
+<div class="abcrun-box">
+  <MonacoEditor />
+</div>
+
+<style>
+  .abcrun-box {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+</style>
