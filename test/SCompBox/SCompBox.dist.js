2e05bbe5a350e83c460edf95700197a8df8188fa
ghjang <jmxexpert@hotmail.com>
2024-08-04 22:02:38 +0900

    'dist 빌드' 테스트용 SCompBox 컴포넌트 테스트 페이지 추가

---
+++ test/SCompBox/SCompBox.dist.js
@@ -0,0 +1,28 @@
+
+import SCompBox from '/build/dist/default/SCompBox.js';
+
+const sCompBox = new SCompBox({
+    target: document.querySelector('#s-comp-box-container'),
+    props: {
+        customElementConfigBasePath: '/build/dist/default',
+
+        compJsBundleBasePath: '/build/dist/default',
+        customCompJsBundleBasePath: '/build/dist/custom',
+
+        compProps: {
+            AbcRun: {
+                editorResourcePath: "/vendor/monaco-editor/browser-rollup-custom/dist"
+            },
+            Marquee: {
+                text: 'Hello, Marquee!'
+            },
+            PyRun: {
+                pyodideIndexURL: "/build/dist/vendor/pyodide",
+                editorResourcePath: "/vendor/monaco-editor/browser-rollup-custom/dist",
+                code: 'print("Hello, Pyodide!")'
+            }
+        }
+    }
+});
+
+window.addEventListener('beforeunload', () => sCompBox.$destroy());
