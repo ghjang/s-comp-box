02a02bf2e285d73d4bf3b22fd62634be862b8731
ghjang <jmxexpert@hotmail.com>
2024-09-08 14:00:13 +0900

    'trapFocus' 액션 타입스크립트화

--- src/common/action/trapFocus.ts
+++ src/common/action/trapFocus.ts
@@ -1,30 +1,40 @@
-export function trapFocus(node) {
-  const previous = document.activeElement;
+import { as } from "../util";
 
-  function focusable() {
+interface TrapFocusAction {
+  destroy(): void;
+}
+
+interface ConditionalTrapFocusOptions {
+  predicate: (() => boolean) | boolean;
+}
+
+export function trapFocus(node: HTMLElement): TrapFocusAction {
+  const previous = as<HTMLElement>(document.activeElement);
+
+  function focusable(): HTMLElement[] {
     return Array.from(
-      node.querySelectorAll(
+      node.querySelectorAll<HTMLElement>(
         'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
       )
     );
   }
 
-  function handleKeydown(event) {
+  function handleKeydown(event: KeyboardEvent): void {
     if (event.key !== "Tab") return;
 
     // NOTE: '포커스'를 실제 가진 요소가 'Shadow DOM' 하위에 있을 경우에
     //       'document.activeElement'는 'Shadow DOM 호스트' 요소를 반환한다.
-    let current = document.activeElement;
+    let current = as<HTMLElement>(document.activeElement);
 
     // NOTE: 'Shadow DOM' 호스트 요소일 경우 'Shadow DOM' 내부의 '포커스'를 실제 가진 요소를 찾는다.
     //       'Shadow DOM'이 '중첩'되어 있을 경우에도 정상적으로 처리된다.
-    while (current && current.shadowRoot && current.shadowRoot.activeElement) {
-      current = current.shadowRoot.activeElement;
+    while (current?.shadowRoot?.activeElement) {
+      current = as<HTMLElement>(current.shadowRoot.activeElement);
     }
 
     const elements = focusable();
-    const first = elements.at(0);
-    const last = elements.at(-1);
+    const first = elements[0];
+    const last = elements[elements.length - 1];
 
     if (event.shiftKey && current === first) {
       last.focus();
@@ -49,8 +59,11 @@ export function trapFocus(node) {
   };
 }
 
-export function conditionalTrapFocus(node, { predicate }) {
-  let shouldTrapFocus;
+export function conditionalTrapFocus(
+  node: HTMLElement,
+  { predicate }: ConditionalTrapFocusOptions
+): TrapFocusAction | undefined {
+  let shouldTrapFocus: boolean;
 
   if (typeof predicate === "function") {
     shouldTrapFocus = predicate();
